85a9e8b395179753db8eb1687c95a05c62173632
QuickJulian <julian5m597@gmail.com>
2022-07-28 06:31:55 +0300

    Add files via upload
    OK it finally works, for real this time!

--- Script.js
+++ Script.js
@@ -4,8 +4,8 @@ gsap.from('.nav_text', { duration: 1, opacity: 0, delay: 1, stagger: .5 })
 gsap.fromTo(".myimg", {opacity: 0}, {opacity: 1, duration: 1, delay: 1.5});
 gsap.from('.myimg', { duration: 1, x: '-100vw', delay: 1, ease: 'power.in' })
 
-gsap.from('.Anim-1', { duration: 1, x: '100vw', delay: 1.5,stagger: 0.9, ease: 'power1.in' })
-gsap.from('.Anim-2', { duration: 1, x: '100vw', delay: 4,stagger: 0.5, ease: 'power1.in' })
+gsap.from('.Anim-1', { duration: 1, y: '100vh', delay: 1.5,stagger: 0.9, ease: 'power1.in' })
+gsap.from('.Anim-2', { duration: 1, y: '100vh', delay: 4,stagger: 0.5, ease: 'power1.in' })
 
 
 
